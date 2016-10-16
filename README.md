599f28887fd302488f899b3b94cb4e61f7b7142a
MrCheerful <ken@melax.com>
2016-10-16 16:09:18 -0600

    initial submission

---
+++ README.md
@@ -0,0 +1,106 @@
+# Getting-and-Cleaning-Data---Course-Project
+==================================================================
+Tidying of Dataset Human Activity Recognition Using Smartphones
+Dataset Addendum 1.0
+Ken Melax
+16 Oct. 2016
+==================================================================
+From the original experiment data the dataset is reduced and
+summarized by means of the associated script.
+
+Details of the modifications to the data are described in the 
+code book:  codebook.md
+
+==================================================================
+The reduced and tidy dataset repo includes the following files:
+
+- 'README.md' - this file.
+- 'run_analysis.R' - script for processing the original dataset files.
+- 'data_summary.txt' - output from script.
+- 'CodeBook.md' - description of reduced dataset and steps in transformation.
+
+Note: to concerve space, the original data has not been included with this repository, but may be retrieved from: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip if required.
+
+==================================================================
+Requirements for script operation
+- script was prepared and run using R Studio, 64 bit edition
+- libraries plyr, dplyr and reshape2 are required to be pre-installed
+- R workspace to be "/UCI HAR Dataset" folder with the data in subfolders /test and /train
+- the script checks for the required datafiles and reports if any files or directories are missing.
+
+Note: further details on the script are included as comments within the script.
+
+
+
+==================================================================
+Readme from original dataset
+==================================================================
+Human Activity Recognition Using Smartphones Dataset
+Version 1.0
+==================================================================
+Jorge L. Reyes-Ortiz, Davide Anguita, Alessandro Ghio, Luca Oneto.
+Smartlab - Non Linear Complex Systems Laboratory
+DITEN - Università degli Studi di Genova.
+Via Opera Pia 11A, I-16145, Genoa, Italy.
+activityrecognition@smartlab.ws
+www.smartlab.ws
+==================================================================
+
+The experiments have been carried out with a group of 30 volunteers within an age bracket of 19-48 years. Each person performed six activities (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING) wearing a smartphone (Samsung Galaxy S II) on the waist. Using its embedded accelerometer and gyroscope, we captured 3-axial linear acceleration and 3-axial angular velocity at a constant rate of 50Hz. The experiments have been video-recorded to label the data manually. The obtained dataset has been randomly partitioned into two sets, where 70% of the volunteers was selected for generating the training data and 30% the test data. 
+
+The sensor signals (accelerometer and gyroscope) were pre-processed by applying noise filters and then sampled in fixed-width sliding windows of 2.56 sec and 50% overlap (128 readings/window). The sensor acceleration signal, which has gravitational and body motion components, was separated using a Butterworth low-pass filter into body acceleration and gravity. The gravitational force is assumed to have only low frequency components, therefore a filter with 0.3 Hz cutoff frequency was used. From each window, a vector of features was obtained by calculating variables from the time and frequency domain. See 'features_info.txt' for more details. 
+
+For each record it is provided:
+======================================
+
+- Triaxial acceleration from the accelerometer (total acceleration) and the estimated body acceleration.
+- Triaxial Angular velocity from the gyroscope. 
+- A 561-feature vector with time and frequency domain variables. 
+- Its activity label. 
+- An identifier of the subject who carried out the experiment.
+
+The dataset includes the following files:
+=========================================
+
+- 'README.txt'
+
+- 'features_info.txt': Shows information about the variables used on the feature vector.
+
+- 'features.txt': List of all features.
+
+- 'activity_labels.txt': Links the class labels with their activity name.
+
+- 'train/X_train.txt': Training set.
+
+- 'train/y_train.txt': Training labels.
+
+- 'test/X_test.txt': Test set.
+
+- 'test/y_test.txt': Test labels.
+
+The following files are available for the train and test data. Their descriptions are equivalent. 
+
+- 'train/subject_train.txt': Each row identifies the subject who performed the activity for each window sample. Its range is from 1 to 30. 
+
+- 'train/Inertial Signals/total_acc_x_train.txt': The acceleration signal from the smartphone accelerometer X axis in standard gravity units 'g'. Every row shows a 128 element vector. The same description applies for the 'total_acc_x_train.txt' and 'total_acc_z_train.txt' files for the Y and Z axis. 
+
+- 'train/Inertial Signals/body_acc_x_train.txt': The body acceleration signal obtained by subtracting the gravity from the total acceleration. 
+
+- 'train/Inertial Signals/body_gyro_x_train.txt': The angular velocity vector measured by the gyroscope for each window sample. The units are radians/second. 
+
+Notes: 
+======
+- Features are normalized and bounded within [-1,1].
+- Each feature vector is a row on the text file.
+
+For more information about this dataset contact: activityrecognition@smartlab.ws
+
+License:
+========
+Use of this dataset in publications must be acknowledged by referencing the following publication [1] 
+
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+
+This dataset is distributed AS-IS and no responsibility implied or explicit can be addressed to the authors or their institutions for its use or misuse. Any commercial use is prohibited.
+
+Jorge L. Reyes-Ortiz, Alessandro Ghio, Luca Oneto, Davide Anguita. November 2012.
---
+++ run_analysis.R
@@ -0,0 +1,174 @@
+##
+##  Course Project for John Hopkins - Getting and Cleaning Data
+##  by Ken Melax  (aka Mr Cheerful on GitHub)
+##  14 October 2016  
+##
+##  -- FILE LOCATIONS --
+##  This script assumes that the session working directory
+##  is "../UCI HAR Dataset" where the datasets are in sub directories
+##  where the naming is as follows
+##    <setname>/subject_<setname>  - contains vector of subject of each observation
+##    <setname>/X_<setname>     - contains table with variables in columns and rows of individual observations
+##    <setname>/y_<setname>     - contains vector of the activity corresponding to each observation
+##
+##  Also required for this script are the following files in the working directory
+##    activity_labels.txt  - contains table of activity index to activity description
+##    features.txt   - contains vector listing the discriptions of each variable/column
+##
+##  -- Script --
+##
+##  Load any libraries necessary
+library(plyr)
+library(dplyr)
+library(reshape2)
+
+## Verify existance of datasets and required files
+## This step is not required, but will make it clear if there
+## is a problem with the script cannot find the necessary files
+
+## Specify Datasets as vector for merging
+#datasets <- c("dev1","dev2")    # Test datasets for code development
+datasets <- c("test","train")   # Production datasets for report
+
+## Specify column headings file
+#featurefile <- "features_dev.txt"    # Test features file for development
+featurefile <- "features.txt"        # Production features file
+
+## Specify data storage file naming convention as list for test
+fileforms <- c("subject_", "X_", "y_")
+
+## Set up indicator for stopping for file error.
+fileerror <- FALSE
+
+## Test for existance of the files containing the metadata
+if(!file.exists("activity_labels.txt")){
+      print("activity_labels.txt not found")
+      fileerror <- TRUE
+      }
+if(!file.exists(featurefile)){
+      print("features.txt not found")
+      fileerror <- TRUE
+      }
+
+## Set up function for checking that all necessary files exist in a data folder
+## Function loops through vector, testing that each of the files exists for a dataset.
+datafilestest <- function(setname, ff){
+      for (xx in ff){
+            if(!file.exists(paste(setname, "/", xx, setname,".txt", sep=""))){
+                  message(setname, "/", xx, setname, ".txt not found")
+                  fileerror <- TRUE
+            }
+      }
+      return
+}      
+
+## Loop through the datasets, check that the folder exists, and if so check the files.
+for (ds in datasets){
+      if(!file.exists(ds)){
+            message("folder ",ds," not found")
+            fileerror <- TRUE
+      }
+      else {datafilestest(ds, fileforms)     }
+}
+
+## If the files are not correctly located, raise an alert
+if (fileerror == TRUE){ print(
+      "Warning - issues exist regarding file locations.  See notes to correct."
+      )}
+
+## Load in the activity metadata, assign column names and set as factors.
+activityMeta <- read.table("activity_labels.txt",header = FALSE, as.is = TRUE)
+names(activityMeta) <- c("id","activity")
+activityMeta$activity <- factor(activityMeta$activity,levels = activityMeta$activity)
+
+## Load in the variable (aka feature) names list, assign column names
+variablesMeta <- read.table(featurefile,header = FALSE, as.is = TRUE)
+names(variablesMeta) <- c("id","variable") 
+
+## -- PROJECT REQUIREMENT #2 --
+## Determine the variables / measures for extraction and reporting
+## The project assignment indicates to extract "ONLY the measurements 
+## on the mean and standard deviation for each measurement."
+## Hence only mean() and std() for each measurement vector is required
+## (The assignment does not ask for the meanFreq, gravityMean, etc. )
+
+## A column is added to the metadata file which contains a boolean 
+## to indicate whether the variable in the row is to be kept or not.
+variablesMeta$extract <- grepl("mean\\(\\)|std\\(\\)",variablesMeta$variable)
+
+## A vector is created listing only the kept variables, to be used when melting.
+varsExtracted <- variablesMeta[variablesMeta$extract==TRUE, "variable" ]
+
+## -- PROJECT REQUIREMENT #1 --
+## Load and merge the datasets
+
+## Load datasets - note on this implementation.
+## the dataset names are loaded by looping over the files and folders
+## and assigning each load into a object whose name is constructed 
+## using an paste0() statement inside an assign() function. 
+for (ff in fileforms){
+      for (ds in datasets){
+            assign(paste0(ff,ds),read.table(paste0(ds,"/",ff,ds,".txt"),
+                                            header = FALSE, as.is = TRUE))
+      }
+}
+
+## Merge observation datasets.  
+## (I couldn't determine how to get rbind to accept a construction of 
+## the dataset names, hence the merges are explicitly defined)
+#x_c <- rbind(x_dev1, x_dev2)      # row bind / combine the development measurement data
+#y_c <- rbind(y_dev1, y_dev2)      # row bind / combine the development activity data
+#subject_c <- rbind(subject_dev1, subject_dev2)  # combine the development subject data
+
+x_c <- rbind(X_test, X_train)
+y_c <- rbind(y_test, y_train)
+subject_c <- rbind(subject_test, subject_train)
+
+## -- PROJECT REQUIREMENT #4 --
+## Note: I chose keep the variable names identical to the raw data names.
+## Given the complexity of the names, introducing shortened versions
+## of the names would, in my opinion, create confusion for tracing the data
+## from source to summary.
+##
+## Apply the variable names as column labels to the combined measurement data set.
+names(x_c) <- variablesMeta$variable
+
+## Remove the unnecessary measurement columns
+for (x in length(variablesMeta$variable):1){
+      if (variablesMeta$extract[x] == FALSE) {
+            x_c[,x]<- NULL
+      }
+}
+
+## Combine columns for subject and activity with the measurement data
+## Dataset "datac" is the compiled dataset in "wide" form
+datac <- cbind(subject_c$V1, y_c$V1, x_c)
+
+## Fix the column names for newly added columns
+names(datac)[names(datac)=="y_c$V1"] = "activity"
+names(datac)[names(datac)=="subject_c$V1"] = "subject"
+
+## -- PROJECT REQUIREMENT #3 --
+## Set up the activity column as factor with descriptive labels applied
+datac$activity <- factor(datac$activity, labels = activityMeta$activity)
+
+## -- PROJECT REQUIREMENT #5 --
+## Convert to long form data
+datalong <- melt(datac, c("subject","activity"), varsExtracted, 
+                 variable.name = "variable", value.name = "value" )
+
+## Make a summary dataset with averages by variable & activity & subject
+## Note: the summary dataset is sorted by variable, then activity and subject
+## as this is potentially most convenient for future work with the data.
+## (sorting by subject, activity and then variable would be easier for 
+## quality control checking of the data set, and is readily done by 
+## adjusting the ddply statement's argument in the next line. 
+datasummary <- ddply( datalong, .(variable, activity, subject), 
+                      summarize, avg = mean(value) )
+
+## Save the tidy, long dataset.  (First sorted for convenience of user.)
+datalongsorted <- arrange(datalong, subject, activity, variable)
+write.table(datalongsorted, file = "data_long.txt", row.names = FALSE)
+
+## Save the summary dataset.
+write.table(datasummary, file = "data_summary.txt", row.names = FALSE)
